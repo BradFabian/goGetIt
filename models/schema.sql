bd9e79e9bd3ee7ab08be6852c881a7edad3a6cef
Brian <rac3k@aol.com>
2018-08-23 18:30:30 -0400

    it works and i dont know how i fixed it

--- models/schema.sql
+++ models/schema.sql
@@ -1,19 +1,19 @@
-DROP DATABASE IF EXISTS goGetIt_db;
 CREATE DATABASE goGetIt_db;
 
 USE goGitIt_db;
 
 CREATE TABLE products(
-    item_id INT AUTO_INCREMENT NOT NULL,
+    ID INT NOT NULL AUTO_INCREMENT,
     product_name VARCHAR(45) NOT NULL,
     product_quantity INT(20) NOT NULL,
     quantity_required INT(20) NOT NULL,
-    PRIMARY KEY(item_id)
+    PRIMARY KEY(ID)
 );
 
 SELECT * FROM products;
 
 INSERT INTO products (product_name, product_quantity, quantity_required)
-VALUES ("Onions", 5, 3),
+VALUES 
+    ("Onions", 5, 3),
     ("Peppers", 3, 5),
     ("Water(16oz bottles)", 10, 10);
