141b026dc3fdc1d580edcd69c9ffd013f257f0ca
Brian <rac3k@aol.com>
2018-08-23 17:57:16 -0400

    created table

--- models/schema.sql
+++ models/schema.sql
@@ -2,3 +2,9 @@ DROP DATABASE IF EXISTS goGetIt_db;
 CREATE DATABASE goGetIt_db;
 
 USE goGitIt_db;
+
+CREATE TABLE(
+    item_id INT AUTO_INCREMENT NOT NULL,
+    product_name VARCHAR(45) NOT NULL,
+    PRIMARY KEY(item_id)
+);
