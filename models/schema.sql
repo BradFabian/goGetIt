3e7d5fe045e59d45b2ebb9a167236d9bf5a713e6
Brian <rac3k@aol.com>
2018-08-23 18:05:59 -0400

    added products to table

--- models/schema.sql
+++ models/schema.sql
@@ -3,10 +3,17 @@ CREATE DATABASE goGetIt_db;
 
 USE goGitIt_db;
 
-CREATE TABLE(
+CREATE TABLE products(
     item_id INT AUTO_INCREMENT NOT NULL,
     product_name VARCHAR(45) NOT NULL,
     product_quantity INT(20) NOT NULL,
     quantity_required INT(20) NOT NULL,
     PRIMARY KEY(item_id)
 );
+
+SELECT * FROM products;
+
+INSERT INTO products (product_name, product_quantity, quantity_required)
+VALUES ("Onions", 5, 3),
+    ("Peppers", 3, 5),
+    ("Water(16oz bottles)", 10, 10);
