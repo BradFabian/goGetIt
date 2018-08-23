6bfed7d71da0344e877d3f619dd6e69d3cda68de
Brian <rac3k@aol.com>
2018-08-23 18:00:12 -0400

    updated table

--- models/schema.sql
+++ models/schema.sql
@@ -6,5 +6,7 @@ USE goGitIt_db;
 CREATE TABLE(
     item_id INT AUTO_INCREMENT NOT NULL,
     product_name VARCHAR(45) NOT NULL,
+    product_quantity INT(20) NOT NULL,
+    quantity_required INT(20) NOT NULL,
     PRIMARY KEY(item_id)
 );
