ad5b5bf60008c55e6dd5bf9f342577c6d4d3d6c3
Brian Racek <rac3k@aol.com>
2018-08-25 22:33:55 -0400

    table created

--- models/schema.sql
+++ models/schema.sql
@@ -5,5 +5,10 @@ USE goGetIt_db;
 
 CREATE TABLE products(
     ID INT AUTO_INCREMENT NOT NULL,
+    product_name VARCHAR(25) NOT NULL,
+    product_quantity INT(45) NOT NULL,
+    quantity_needed INT(45) NOT NULL,
+    quantity_ordered INT(45) NOT NULL,
+    primary key(ID)
     
-    )
+    );
