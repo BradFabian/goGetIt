7422cb6b7809091cdda9e7c6ba60d860180e1c21
MondoBiscuit <bradleofabian@gmail.com>
2018-08-30 11:30:21 -0400

    added login.js and code

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,15 @@
 var db = require("../models");
 
 module.exports = function(app) {
+  //user login redirect//
+  app.post("/api/login", passport.authenticate("local"), function(req, res) {
+    // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
+    // So we're sending the user back the route to the members page because the redirect will happen on the front end
+    // They won't get this or even be able to access this page if they aren't authed
+    res.json("/member");
+  });
+
+
   // Get all examples
   app.get("/api/inventory", function(req, res) {
     
