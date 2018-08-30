e59e5fe53db10ad15d1608ac612352b1db1843a3
MondoBiscuit <bradleofabian@gmail.com>
2018-08-30 11:03:10 -0400

    added greeting javascript for member page

---
+++ public/js/greeting.js
@@ -0,0 +1,15 @@
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+if (hourNow > 18) {
+   greeting = 'Good Evening, Tomato Gardens!';
+} else if (hourNow > 12){
+    greeting = 'Good Afternoon, Tomato Gardens!';
+} else if (hourNow > 0){
+    greeting = 'Good Morning, Tomato Gardens!';
+} else {
+    greeting = 'Welcome, Tomato Gardens!';
+}
+
+document.write('<span>' + greeting + '</span>');
