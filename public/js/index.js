e20c266da11c777e5bfd708020337febbc2d110d
MondoBiscuit <bradleofabian@gmail.com>
2018-08-30 20:38:56 -0400

    fixed css options

--- public/js/index.js
+++ public/js/index.js
@@ -28,9 +28,30 @@ $.get("/api/inventory", function(data) {
                           var idRow = '<td>' + data[i].id + '</td>';
                           var productRow = '<td>' + data[i].product_name + '</td>';
                           var stockRow = '<td>' + data[i].product_quantity + '</td>';
-                          var neededRow = '<td>' + data[i].product_needed + '</td>';
-                          var quantityRow = '<td>' + data[i].quantity_ordered + '</td>';
-                          var deleteBut =   '<button type="button" class="delete btn btn-danger">'+ "Delete" + '</button>'  
+                         // var updateRow = '<td>' + data[i].product_needed + '</td>';
+                          var quantityRow = `<td> 
+                          <div class='form-group'>
+                          <select class="chosen-select" id="selector">
+                            <option>1</option>
+                            <option>2</option>
+                            <option>3</option>
+                            <option>4</option>
+                            <option>5</option>
+                            <option>6</option>
+                            <option>7</option>
+                            <option>8</option>
+                            <option>9</option>
+                            <option>10</option>
+                            <option>11</option>
+                            <option>12</option>
+                            <option>13</option>
+                            <option>14</option>
+                            <option>15</option>
+                          </select>
+                          </div>
+                         </td>`;
+                         var updateBut =   "<td>" +'<button type="button" class="delete btn btn-success">'+ "Update" + '</button>'+ "</td>" 
+                          var deleteBut =   "<td>" + '<button type="button" class="delete btn btn-danger">'+ "Delete" + '</button>'+ "</td>" 
   
                          
                           
@@ -39,8 +60,9 @@ $.get("/api/inventory", function(data) {
                           tr.append(idRow);
                           tr.append(productRow);
                           tr.append(stockRow);
-                          tr.append(neededRow);
+                         // tr.append(neededRow);
                           tr.append(quantityRow);
+                          tr.append(updateBut);
                           tr.append(deleteBut);
                           // Append the table row to the tbody element
                           tbody.append(tr)
