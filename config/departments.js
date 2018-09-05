8c424db73599b1908f4b99bc3af44cf43137e01f
monterocjaime <monterocjaime@gmail.com>
2018-09-05 19:15:24 -0400

    deparments js added, contains departments of the restaurant

---
+++ config/departments.js
@@ -0,0 +1,7 @@
+// List of departments
+
+module.exports = {
+	    0: {"value":"Kitchen", "text":"Kitchen"},
+	    2: {"value":"Cleaning", "text":"Cleaning"},
+	    3: {"value":"Office", "text":"Office"},
+};
