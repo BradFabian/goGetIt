dd5b60cf7d4d3d3ff09b9ed3af447349338325d9
Brian Racek <rac3k@aol.com>
2018-09-05 22:00:17 -0400

    apiRoutes needed in product.js

--- routes/product.js
+++ routes/product.js
@@ -1,6 +1,33 @@
 const express = require('express');
 const router = express.Router();
 const request = require('request');
+var db = require("../models");
+var passport = require("../config/passport");
+
+
+module.exports = function(app) {
+	//user login redirect//
+	app.post("/api/login", passport.authenticate("local"), function(req, res) {
+	  // Since we're doing a POST with javascript, we can't actually redirect that post into a GET request
+	  // So we're sending the user back the route to the members page because the redirect will happen on the front end
+	  // They won't get this or even be able to access this page if they aren't authed
+	  res.json("/member");
+	});
+  
+  
+	// Get all examples
+	app.get("/controllers/productcontroller", function(req, res) {
+	  db.Products.findAll({}).then(function(dbgoGetIt) {
+		res.json(dbgoGetIt);
+	  });
+	});
+
+ 
+	app.post("/api/member", passport.authenticate("local"), function(req, res) {
+	  res.json("/add");
+	});
+  };
+
 
 // Require controller modules
 var productController = require('../controllers/productcontroller');
